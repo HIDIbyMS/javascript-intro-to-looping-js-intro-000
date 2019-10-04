81b23adcd540882f288ac19cb0fcf7238181c763
Michelle Sharpe <hidibyms@gmail.com>
2019-10-04 16:21:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -7,7 +7,7 @@ function forLoop(array){
     if (i === 1) {
       console.log("I am " + i + " strange loop.");
     } else {
-      console.log("I am " + i + " stranges.");
+      console.log("I am " + i + " strange loops.");
     }
   }
 }
